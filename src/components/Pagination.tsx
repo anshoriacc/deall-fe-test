308f61f556f21ae985b3a3aca1bb12e6e2a39357
achmad anshori <utd.rik@gmail.com>
2023-02-15 12:42:05 +0700

    add: Pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import { PaginationProps } from 'src/types';
+
+import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
+
+function Pagination({
+  page,
+  totalPage,
+  nextPage,
+  previousPage,
+}: PaginationProps) {
+  return (
+    <div className="px-4 flex justify-end items-center gap-4 text-lg">
+      <button
+        onClick={previousPage}
+        disabled={page === 1}
+        className={clsx(
+          'p-2 bg-[#6913d8] text-gray-0 flex justify-center items-center rounded-lg',
+          'disabled:bg-gray-100 disabled:text-gray-500'
+        )}
+      >
+        <MdArrowBackIos />
+      </button>
+      <div>{page}</div>
+      <button
+        onClick={nextPage}
+        disabled={page === totalPage}
+        className={clsx(
+          'p-2 bg-[#6913d8] text-gray-0 flex justify-center items-center rounded-lg',
+          'disabled:bg-gray-100 disabled:text-gray-500'
+        )}
+      >
+        <MdArrowForwardIos />
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
