ccc6872fdadf73849ebb57c2222ff1d2b3d85898
achmad anshori <utd.rik@gmail.com>
2023-02-16 08:23:06 +0700

    fix: styling active NavItem

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -6,14 +6,18 @@ import { NavItemProps } from 'src/types';
 
 export default function NavItem({ href, children, name }: NavItemProps) {
   const router = useRouter();
-  const isActive = router.asPath === href;
+  const isActive =
+    href !== '/' ? router.asPath.startsWith(href) : router.asPath === href;
+  console.log(router);
 
   return (
     <Link
       href={href}
       title={name}
       className={clsx(
-        isActive ? 'font-bold bg-[#6913d8] text-gray-0' : 'hover:bg-gray-100 dark:hover:bg-gray-800',
+        isActive
+          ? 'font-bold bg-[#6913d8] text-gray-0'
+          : 'hover:bg-gray-100 dark:hover:bg-gray-800',
         'p-2 rounded-lg flex gap-2 items-center transition-all'
       )}
     >
